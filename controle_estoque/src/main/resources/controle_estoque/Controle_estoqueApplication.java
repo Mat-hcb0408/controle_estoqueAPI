12873044d1ffdf4c349908dc6c47b57ddffb9968
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-05-22 23:06:57 -0300

    Add files via upload

---
+++ controle_estoque/src/main/resources/controle_estoque/Controle_estoqueApplication.java
@@ -0,0 +1,11 @@
+package com.example.controle_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle_estoqueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Controle_estoqueApplication.class, args);
+    }
+}
