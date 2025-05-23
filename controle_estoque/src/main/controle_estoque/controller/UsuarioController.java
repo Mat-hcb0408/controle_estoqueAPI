f36a9ca175e4cc9d7c5b9ca943d431d323908d63
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-05-22 21:02:47 -0300

    Update UsuarioController.java

--- controle_estoque/src/main/controle_estoque/controller/UsuarioController.java
+++ controle_estoque/src/main/controle_estoque/controller/UsuarioController.java
@@ -4,19 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.controle_estoque.model.UsuarioModel;
 import com.example.controle_estoque.services.UsuarioService;
-
-import io.micrometer.core.ipc.http.HttpSender.Response;
-
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -50,5 +48,14 @@ public class UsuarioController {
         usuarioModel.setId_usuario(id);
 
         return ResponseEntity.ok(service.salvar(usuarioModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
+        if(!service.buscarId(id).isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
